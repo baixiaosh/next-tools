6d447b820ddc244a85d1ac6d1c380065e55c3c38
柏小生先生 <782179350@qq.com>
2021-09-17 11:56:00 +0800

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import './index.less';
 class Index extends React.Component {
     static async getInitialProps({ req }) {
         console.log(req.headers['user-agent']);
-        return { test: 'sb' };
+        return { test: '开挂' };
     }
     render() {
         return (
@@ -15,7 +15,7 @@ class Index extends React.Component {
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 </Head>
                 <div className="xxx">
-                    <p>Hello word {this.props.test}</p>
+                    <p>你的人生从此开始 {this.props.test}</p>
                 </div>
             </div>
         );
